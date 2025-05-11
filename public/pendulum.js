40f4297e6dcc59ff6526216a6e3a52888c08fff1
pop161516 <162404430+pop161516@users.noreply.github.com>
2025-05-12 02:08:54 +1000

    test

---
+++ public/pendulum.js
@@ -0,0 +1,84 @@
+function Pendulum(angle1, angle2, length1, length2) {
+  this.x0 = null; // Will be set later, needs context of canvas width
+  this.y0 = null; // Will be set later, needs context of canvas height
+  this.angle1 = angle1;
+  this.angle2 = angle2;
+  this.angleV1 = 0;
+  this.angleV2 = 0;
+  this.angleA1 = 0;
+  this.angleA2 = 0;
+  this.m1 = 1;
+  this.m2 = 1;
+  this.length1 = length1;
+  this.length2 = length2;
+}
+
+Pendulum.prototype.setCanvasDimensions = function(width, height) {
+  this.x0 = width / 2;
+  this.y0 = height / 2;
+};
+
+Pendulum.prototype.update = function() {
+  const g = 9.81; // Assuming gravity constant
+
+  const sin = Math.sin;
+  const cos = Math.cos;
+
+  this.x1 = this.x0 + this.length1 * sin(this.angle1);
+  this.y1 = this.y0 + this.length1 * cos(this.angle1);
+  this.x2 = this.x1 + this.length2 * sin(this.angle2);
+  this.y2 = this.y1 + this.length2 * cos(this.angle2);
+
+  this.angleA1 =
+    (-g * (2 * this.m1 + this.m2) * sin(this.angle1) -
+      this.m2 * g * sin(this.angle1 - 2 * this.angle2) -
+      2 *
+        sin(this.angle1 - this.angle2) *
+        this.m2 *
+        (this.angleV2 * this.angleV2 * this.length2 +
+          this.angleV1 * this.angleV1 * this.length1 * cos(this.angle1 - this.angle2))) /
+    (this.length1 *
+      (2 * this.m1 +
+        this.m2 -
+        this.m2 * cos(2 * this.angle1 - 2 * this.angle2)));
+
+  this.angleA2 =
+    (2 *
+      sin(this.angle1 - this.angle2) *
+      (this.angleV1 * this.angleV1 * this.length1 * (this.m1 + this.m2) +
+        g * (this.m1 + this.m2) * cos(this.angle1) +
+        this.angleV2 *
+          this.angleV2 *
+          this.length2 *
+          this.m2 *
+          cos(this.angle1 - this.angle2))) /
+    (this.length2 *
+      (2 * this.m1 +
+        this.m2 -
+        this.m2 * cos(2 * this.angle1 - 2 * this.angle2)));
+
+  this.angleV1 += this.angleA1;
+  this.angleV2 += this.angleA2;
+
+  this.angle1 += this.angleV1;
+  this.angle2 += this.angleV2;
+
+  this.angleV1 *= 0.996;
+  this.angleV2 *= 0.996;
+};
+
+Pendulum.prototype.display = function(ctx) {
+  ctx.beginPath();
+  ctx.moveTo(this.x0, this.y0);
+  ctx.lineTo(this.x1, this.y1);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.moveTo(this.x1, this.y1);
+  ctx.lineTo(this.x2, this.y2);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(this.x2, this.y2, 10, 0, 2 * Math.PI);
+  ctx.fill();
+};
--- public/testScript.js
+++ public/testScript.js
@@ -49,7 +49,8 @@ const ctx = canvas.getContext('2d');
 
 //---------------------------------------------------------------------------------//
 //sketch!
-  let g = 1;
+/*
+  let g = 1; 
 
   let x0, y0;
   let x1, y1;
@@ -133,3 +134,61 @@ const ctx = canvas.getContext('2d');
 
   setup();
   draw(); // Start the animation
+
+*/
+
+//-----//
+//sketch2
+let width = window.innerWidth;
+let height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
+
+let g = 1;
+let pendulums = [];
+let num = 10;
+
+function setup() {
+  for (let i = 0; i < num; i++) {
+    let angle1 = Math.PI / 4 + i * 0.01;
+    let angle2 = Math.PI;
+    pendulums[i] = new Pendulum(angle1, angle2, 100, 100);
+    pendulums[i].setCanvasDimensions(width, height);
+    pendulums[i].currentG = g; // Initialize gravity for each pendulum
+  }
+}
+
+function draw() {
+  requestAnimationFrame(draw);
+  ctx.fillStyle = 'rgb(220, 220, 220)'; // Equivalent to background(220)
+  ctx.fillRect(0, 0, width, height);
+
+  if (frameCount % 1000 < 500) {
+    g = 1;
+  } else {
+    g = -1;
+  }
+  console.log(g);
+
+  for (let i = 0; i < num; i++) {
+    pendulums[i].currentG = g; // Update gravity for each pendulum
+    pendulums[i].update();
+    pendulums[i].display(ctx);
+  }
+
+  frameCount++;
+}
+
+let frameCount = 0;
+setup();
+draw();
+
+window.addEventListener('resize', () => {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  canvas.width = width;
+  canvas.height = height;
+  for (let i = 0; i < num; i++) {
+    pendulums[i].setCanvasDimensions(width, height);
+  }
+});
