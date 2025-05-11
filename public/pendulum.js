f2d590f93a6bd8939f7a39eb569f811f8334a312
pop161516 <162404430+pop161516@users.noreply.github.com>
2025-05-12 08:36:53 +1000

    test

--- public/pendulum.js
+++ public/pendulum.js
@@ -63,8 +63,8 @@ Pendulum.prototype.update = function() {
   this.angle1 += this.angleV1;
   this.angle2 += this.angleV2;
 
-  this.angleV1 *= 0.98;
-  this.angleV2 *= 0.98;
+  this.angleV1 *= 0.9999;
+  this.angleV2 *= 0.9999;
 };
 
 Pendulum.prototype.display = function(ctx) {
