485d21a88d724416d6594dec75c438c98b0bfea1
pop161516 <162404430+pop161516@users.noreply.github.com>
2025-05-12 00:16:48 +1000

    reset

--- public/script.js
+++ public/script.js
@@ -7,27 +7,17 @@ cnv.height = innerHeight
 
 const ctx = cnv.getContext (`2d`)
 
-let gyroscope = new Gyroscope({ frequency: 60 });
-
-gyroscope.addEventListener("reading", (e) => {
-  console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
-  console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
-  console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
-});
-gyroscope.start();
-
-
 const draw_frame = ms => {
-   ctx.fillStyle = `red`
+   ctx.fillStyle = `turquoise`
    ctx.fillRect (0, 0, innerWidth, innerHeight)
 
    const seconds = (ms / 1000)
+   console.log (seconds.toFixed (2))
+
    requestAnimationFrame (draw_frame)
 }
 
 draw_frame ()
-
-
 
 onresize = () => {
    cnv.width = innerWidth
