bab0769656bbacb474047057a33f92ca816df307
pop161516 <162404430+pop161516@users.noreply.github.com>
2025-05-12 00:04:31 +1000

    test

--- public/script.js
+++ public/script.js
@@ -8,7 +8,7 @@ cnv.height = innerHeight
 const ctx = cnv.getContext (`2d`)
 
 const draw_frame = ms => {
-   ctx.fillStyle = `turquoise`
+   ctx.fillStyle = `red`
    ctx.fillRect (0, 0, innerWidth, innerHeight)
 
    const seconds = (ms / 1000)
@@ -18,6 +18,16 @@ const draw_frame = ms => {
 }
 
 draw_frame ()
+
+let gyroscope = new Gyroscope({ frequency: 60 });
+
+gyroscope.addEventListener("reading", (e) => {
+  console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
+  console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
+  console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
+});
+gyroscope.start();
+
 
 onresize = () => {
    cnv.width = innerWidth
