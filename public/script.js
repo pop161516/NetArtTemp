05b5ca76a5651739d21b65a6f9e0d0f7f0cfb6a6
pop161516 <162404430+pop161516@users.noreply.github.com>
2025-05-12 00:07:38 +1000

    test3

--- public/script.js
+++ public/script.js
@@ -7,25 +7,6 @@ cnv.height = innerHeight
 
 const ctx = cnv.getContext (`2d`)
 
-const draw_frame = ms => {
-   ctx.fillStyle = `red`
-   ctx.fillRect (0, 0, innerWidth, innerHeight)
-
-   let gyroscope = new Gyroscope({ frequency: 60 });
-
-gyroscope.addEventListener("reading", (e) => {
-  console.log(`Angular velocity along the X-axis ${gyroscope.x}`);
-  console.log(`Angular velocity along the Y-axis ${gyroscope.y}`);
-  console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
-});
-gyroscope.start();
-
-   const seconds = (ms / 1000)
-   requestAnimationFrame (draw_frame)
-}
-
-draw_frame ()
-
 let gyroscope = new Gyroscope({ frequency: 60 });
 
 gyroscope.addEventListener("reading", (e) => {
@@ -34,6 +15,17 @@ gyroscope.addEventListener("reading", (e) => {
   console.log(`Angular velocity along the Z-axis ${gyroscope.z}`);
 });
 gyroscope.start();
+
+
+const draw_frame = ms => {
+   ctx.fillStyle = `red`
+   ctx.fillRect (0, 0, innerWidth, innerHeight)
+
+   const seconds = (ms / 1000)
+   requestAnimationFrame (draw_frame)
+}
+
+draw_frame ()
 
 
 
