b6c9a33a67cb5252e7c73e5d0b40db55d5f4b068
pop161516 <162404430+pop161516@users.noreply.github.com>
2025-05-12 12:12:35 +1000

    aenoudv

--- public/testScript.js
+++ public/testScript.js
@@ -38,7 +38,7 @@ const introElement = document.getElementById('interactiveElement');
     const gamma = event.gamma;
 
     if (introElement) {
-      introElement.style.transform = `translate(-50%, -50%) rotateX(${gamma}deg) rotateZ(${beta - 90}deg) rotateY(${alpha + 90}deg)`;
+      introElement.style.transform = `translate(-50%, -50%) rotateY(${gamma}deg) rotateX(${beta - 90}deg) rotateZ(${alpha + 90}deg)`;
     }
   }
 //--------------------------------------------------//
