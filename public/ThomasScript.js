0f1f3d50e41af06e64f8aaa4bec9e08d0bbf42f3
pop161516 <162404430+pop161516@users.noreply.github.com>
2025-05-13 18:00:08 +1000

    done

--- public/ThomasScript.js
+++ public/ThomasScript.js
@@ -51,6 +51,52 @@
         if (running) setTimeout (note_player, period)
     }
 
+    // define a function that plays a note
+function play_note (note, length) {
+
+    // if the audio context is not running, resume it
+    if (audio_context.state != 'running') init_audio ()
+
+    // create an oscillator
+    const osc = audio_context.createOscillator ()
+
+    // make it a triangle wave this time
+    osc.type            = 'triangle'
+
+    // set the value using the equation 
+    // for midi note to Hz
+    osc.frequency.value = 440 * 2 ** ((note - 69) / 12)
+
+    // create an amp node
+    const amp = audio_context.createGain ()
+
+    // connect the oscillator 
+    // to the amp
+    // to the audio out
+    osc.connect (amp).connect (audio_context.destination)
+
+    // the .currentTime property of the audio context
+    // contains a time value in seconds
+    const now = audio_context.currentTime
+
+    // make a gain envelope
+    // start at 0
+    amp.gain.setValueAtTime (0, now)
+
+    // take 0.02 seconds to go to 0.4, linearly
+    amp.gain.linearRampToValueAtTime (0.4, now + 0.02)
+
+    // this method does not like going to all the way to 0
+    // so take length seconds to go to 0.0001, exponentially
+    amp.gain.exponentialRampToValueAtTime (0.0001, now + length)
+
+    // start the oscillator now
+    osc.start (now)
+
+    // stop the oscillator 1 second from now
+    osc.stop  (now + length)
+}
+
     // this function handles the mouse event
     // when the cursor enters the canvas
     cnv_0.onpointerenter = e => {
