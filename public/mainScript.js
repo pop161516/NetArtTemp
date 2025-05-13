c4e44b04c459cf6ea007e19827494222ac2fdca5
pop161516 <162404430+pop161516@users.noreply.github.com>
2025-05-13 14:21:54 +1000

    test

--- public/mainScript.js
+++ public/mainScript.js
@@ -167,7 +167,7 @@ function draw() {
     // Control frequency based on pendulum's y position
     const y = pendulums[i].y2; // Y position of the second bob
     const minFrequency = 0;  // Minimum frequency (adjust as needed)
-    const maxFrequency = 1000; // Maximum frequency (adjust as needed)
+    const maxFrequency = 1500; // Maximum frequency (adjust as needed)
     const frequency = map(y, 0, height, maxFrequency, minFrequency); //Higher y = lower freq
     oscillators[i].oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
   
